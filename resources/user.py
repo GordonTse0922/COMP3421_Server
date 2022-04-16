d96145fec3570e440b6e1cb29b4d58abf802663d
Gordon Tse <tsehoyin.gordon@gmail.com>
2022-04-16 19:40:26 +0800

    added password col to user table and hashing for signup & login function

--- resources/user.py
+++ resources/user.py
@@ -1,10 +1,14 @@
+from sqlite3 import IntegrityError
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 from marshmallow import ValidationError
-from schema.user import UserSchema
+from schema.user import UserLoginSChema, UserSchema
 from flask import request, jsonify
 from models.user import UserModel
+from werkzeug.security import generate_password_hash, check_password_hash
 
 user_schema = UserSchema(many=False)
+login_schema=UserLoginSChema(many=False)
 users_schema = UserSchema(many=True)
 class User (Resource):
 
@@ -27,12 +31,24 @@ class User (Resource):
             data = user_schema.load(json_data)
         except ValidationError as err:
             return err.messages, 422
-        user = UserModel(data['name'], data['email'])
-        user.add_user()
-        return {
-            'message': 'Insert user success',
-            'user': user_schema.dump(user)
-        }
+        password=data['password']
+        username=data['name']
+        email=data['email']
+        if username and password and email:
+            user = UserModel(username, email, generate_password_hash(password))
+            try:
+                user.add_user()
+            except IntegrityError:
+                return {
+                'message': 'username/email already exist!',
+                # 'user': user_schema.dump(user)
+                }, 400
+
+            return {
+                'message': 'Insert user success',
+                # 'user': user_schema.dump(user)
+            }
+        return {"message": "No input data provided"}, 400
 
     def put(self):
         json_data = request.get_json()
@@ -67,3 +83,26 @@ class Users(Resource):
             'message': 'All users',
             'users': UserModel.get_all_user()
         }
+
+class Login(Resource):
+    def post(self):
+        json_data = request.get_json()
+        if not json_data:
+            return {"message": "No input data provided"}, 400
+        try:
+            data = login_schema.load(json_data)
+        except ValidationError as err:
+            return err.messages, 422
+        password=data['password']
+        email=data['email']
+        user=UserModel.get_user(email)
+        if check_password_hash(user.password,password) and user is not None:
+            return {
+                'meesage': 'Login success'
+            }, 200
+        else:
+            return {
+                'meesage': 'Incorrect Email/Password'
+            }, 400
+
+
--- schema/user.py
+++ schema/user.py
@@ -10,4 +10,9 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
     id= fields.Integer(dump_only=True)
     name= fields.String(required=True, validate=must_not_be_blank)
     email = fields.Email(required=True, validate=must_not_be_blank)
+    password = fields.String(required=True, validate=must_not_be_blank)
     created_at = fields.DateTime(dump_only=True)
+
+class UserLoginSChema(ma.Schema):
+    email = fields.Email(required=True, validate=must_not_be_blank)
+    password = fields.String(required=True, validate=must_not_be_blank)
