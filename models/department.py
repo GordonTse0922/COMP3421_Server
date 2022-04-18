17601516f2c7d329409fca56f73e24a8253d6eaf
Gordon Tse <tsehoyin.gordon@gmail.com>
2022-04-18 16:59:00 +0800

    add 6 departments to db

--- models/department.py
+++ models/department.py
@@ -8,7 +8,7 @@ class DepartmentModel(db.Model):
     id = db.Column(db.Integer, primary_key =True,autoincrement=True)
     name = db.Column(db.String(100), nullable = False, unique = True)
     description = db.Column(db.String(300), nullable= False)
-    posts = db.relationship('PostModel')
+    # posts = db.relationship('PostModel')
 
     def __init__(self, name, description):
         self.name = name
