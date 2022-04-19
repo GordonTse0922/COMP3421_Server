1602401e6e8e7ef12c4975bd7d87fbc5a0ceb13f
Gordon Tse <tsehoyin.gordon@gmail.com>
2022-04-20 01:07:33 +0800

    udpate for deployment config var for security

--- api/__init__.py
+++ api/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     #     origins=[config_object.CORS_ALLOW_ORIGIN], # the domains allowed to access the server
     #     supports_credentials=config_object.CORS_SUPPORTS_CREDENTIALS) # True
     # app.config.from_object(config_object)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dev.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ('DB_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     api=Api(app)
     api.add_resource(User, "/user")
