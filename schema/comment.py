065cab1fd185402ec24e104167a53ef8167212b9
Gordon Tse <tsehoyin.gordon@gmail.com>
2022-04-20 21:02:00 +0800

    changes for local server

--- schema/comment.py
+++ schema/comment.py
@@ -12,7 +12,7 @@ class CommentSchema(ma.SQLAlchemyAutoSchema):
     post_id= fields.Integer(required=True,validate=must_not_be_blank)
     content = fields.String(required=True, validate=must_not_be_blank)
     created_at = fields.DateTime(dump_only=True)
-    user=fields.Nested(UserSchema(only=['id','name']), many=False)
+    user=fields.Nested(UserSchema(only=['id','name']), many=False,dump_only=True)
 
 class CommentOutputSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
